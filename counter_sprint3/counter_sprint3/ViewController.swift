c82b447ce2d5d28e1d10de4033651b032302a381
NikolayDolnik <dolnik.nikolo@gmail.com>
2023-02-21 12:51:17 +0300

    Добавлена кнопка сброса счетчика

--- counter_sprint3/counter_sprint3/ViewController.swift
+++ counter_sprint3/counter_sprint3/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
     var count : Int = 0
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var buttonCount: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
     
     @IBAction func addCount(_ sender: Any) {
         count+=1
         counterLabel.text = "Значение счетчика: \n \(count)"
      //   counterLabel.text
+    }
+    
+    @IBAction func resetCount(_ sender: Any) {
+        counterLabel.text = "Последнее значение счетчика: \n \(count)"
+        count = 0
     }
     
     override func viewDidLoad() {
